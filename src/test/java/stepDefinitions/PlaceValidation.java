e285c2d5210e79e0d9c87fed5c03433a5bd74e18
Devyani <Devyaniparate1996@gmail.com>
2021-06-10 08:12:05 +0530

    Added comments to Place Validation step definition

--- src/test/java/stepDefinitions/PlaceValidation.java
+++ src/test/java/stepDefinitions/PlaceValidation.java
@@ -23,11 +23,13 @@ public class PlaceValidation {
 	Response response;
 	static String entity_id;
 	
+	//Create request with payload using POJO classes
 	@Given("Place payload with {string} {string} {string}")
 	public void place_payload_with(String name, String language, String address) throws IOException {
 		request = given().spec(ReusableMethods.getRequestSpec()).body(TestData.addPlacePayload(name, language, address));
 	}
 	
+	//Send the http request
 	@When("User calls {string} with {string} http request")
 	public Response user_calls_with_http_request(String resource, String method) {
 		APIResources res = APIResources.valueOf(resource);
@@ -48,24 +50,28 @@ public class PlaceValidation {
 		return response;
 	}
 	
+	//Verify that attributes in the response
 	@Then("API responds with {string} as {string}")
 	public void api_responds_with_as(String responseKey, String expectedValue) {
 	    assertEquals(ReusableMethods.getJsonValue(response,responseKey).toString(),expectedValue);
 	    System.out.println(responseKey+" equals "+expectedValue);
 	}
 	
+	//Verify the status code of response
 	@Then("API responds with status code {int}")
 	public void api_responds_with_status_code(int statusCode) {
 	    assertEquals(response.getStatusCode(),statusCode);
 	    System.out.println("Status code is :"+statusCode);
 	}
 	
+	//Verify place is added successfully
 	@Then("Place is added successfully")
 	public void place_is_added_successfully() {
 	    
 	}
 	
-		@Then("Verify {string} contains expected {string} {string} using {string}")
+	//Verify that the mentioned entity contains the attribute key value using the mentioned resource
+	@Then("Verify {string} contains expected {string} {string} using {string}")
 	public void verify_contains_expected_using(String entity, String attribute_key, String attribute_value, String resource) throws IOException {
 		//get request spec
 		entity_id = ReusableMethods.getJsonValue(response,entity);
@@ -74,7 +80,8 @@ public class PlaceValidation {
 		assertEquals(ReusableMethods.getJsonValue(response, attribute_key),attribute_value);
 		System.out.println("Actual "+attribute_key+" value matches the expected one..!");
 	}
-		
+	
+	//Create DeletePlaceAPI payload
 	@Given("Delete place payload")
 	public void delete_place_payload() throws IOException {
 		request = given().spec(ReusableMethods.getRequestSpec()).body(TestData.deletePlacePayload(entity_id));
